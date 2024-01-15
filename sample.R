1364871c9d1a5e4af28899306acfb3c39ab971dd
Alfred <alfredwu2003@gmail.com>
2024-01-15 16:32:11 -0400

    test INLA

--- sample.R
+++ sample.R
@@ -1,5 +1,14 @@
-2 + 3
-3 + 1
-4 + 2
-3 + 3
-4 + 9
+
+n = 100; a = 1; b = 1; tau = 100
+z = rnorm(n)
+eta = a + b*z
+
+scale = exp(rnorm(n))
+prec = scale*tau
+y = rnorm(n, mean = eta, sd = 1/sqrt(prec))
+
+data = list(y=y, z=z)
+formula = y ~ 1+z
+result = inla(formula, family = "gaussian", data = data)
+
+summary(result)
